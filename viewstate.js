9c7bcf6cce87d43eb6823c5a6a1e8333fd667889
Mike <mike@deadliestwebattacks.com>
2013-12-27 09:04:16 -0800

    Show component hierarchy.

--- viewstate.js
+++ viewstate.js
@@ -8,11 +8,12 @@ YUI().use('event', 'node', 'tree', function (Y) {
     var div = Y.one('#resultTree'),
         item = null,
         tree = new Y.Tree(),
-        node = tree.rootNode;
+        node = tree.rootNode,
+        spaces = '....................';
 
     data.forEach(function(d) {
       node.append({id: d.str()});
-      item = Y.Node.create(d.str() + '<br>');
+      item = Y.Node.create(spaces.substring(0, d.depth()) + ' ' + d.str() + '<br>');
       div.append(item);
     });
 //    console.log(tree);
@@ -35,77 +36,3 @@ YUI().use('event', 'node', 'tree', function (Y) {
   Y.one('#decode').on('click', decode);
 
 });
-
-function c() {
-  document.getElementById("results").innerHTML = "";
-}
-
-function deserialize(o) {
-  var e = document.getElementById("results");
-  var s = "invalid";
-
-  if(!capabilityChecks()) {
-    s = "browser does not support Typed Array Specification (http://www.khronos.org/registry/typedarray/specs/latest/)";
-  }
-  else if(undefined != o && "" !== o) {
-    var vs = new ViewState(o);
-    if(vs.isValid()) {
-      vs.consume();
-      var c = vs.components();
-      populateTree(c);
-      s = "";
-    }
-  }
-  e.innerHTML = s;
-}
-
-function populateTree(c) {
-  var tree = new YAHOO.widget.TreeView("resultTree");
-  var rootNode = tree.getRoot();
-
-  var node = rootNode;
-  var parentNode = rootNode;
-  var nodes = [];
-  nodes[0] = rootNode;
-  nodes[1] = rootNode;
-
-  var e = c[0];
-  var prevDepth = 0;
-  var i = 0;
-  while(i < c.length) {
-    e = c[i];
-
-    if(e.depth() > prevDepth) {
-      parentNode = nodes[prevDepth];
-    }
-    else if(e.depth() < prevDepth) {
-      if(undefined == nodes[e.depth()])
-        nodes[e.depth()] = rootNode;
-
-      parentNode = nodes[e.depth()].parent;
-    }
-
-    if(undefined == parentNode)
-      parentNode = rootNode;
-  
-    node = new YAHOO.widget.TextNode(e.str(), parentNode, false);
-
-    nodes[e.depth()] = node;
-    prevDepth = e.depth();
-    ++i;
-  }
-
-  YAHOO.util.Event.on("expand", "click", function(e) {
-      YAHOO.log("Expanding all TreeView  nodes.", "info", "example");
-      tree.expandAll();
-      YAHOO.util.Event.preventDefault(e);
-    });
-    
-    YAHOO.util.Event.on("collapse", "click", function(e) {
-      YAHOO.log("Collapsing all TreeView  nodes.", "info", "example");
-      tree.collapseAll();
-      YAHOO.util.Event.preventDefault(e);
-    });
-
-  tree.draw();
-}
